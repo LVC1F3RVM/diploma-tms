af71f9f521c656bf354c84c5d4c1ced391aa012a
Name <username@gmail.com>
2020-10-09 23:32:22 +0300

    Logo complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Grid, AppBar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import { Link } from "react-router-dom";
 import logo from "../images/logo.png";
@@ -13,11 +12,27 @@ const useStyles = makeStyles({
   },
   container_styles: {
     display: "flex",
-    justifyContent: "space-evenly",
-    
+    justifyContent: "space-between",
+    margin: "0px 205px 0px 205px",
+    padding: "0px 10px 0 10px"
     
   },
+  logo_copy: {
+    display: "flex",
+    flexDirection: "row",
+    textDecoration: "none",
+  },
+  logo_copy_text: {
+    color: "white"
+  },
+  site_title: {
+    margin: 0,
+    textTransform: "uppercase",
+    fontSize: "1em"
+  },
   nav: {
+    display: "flex",
+    justifyContent: "flex-end",
     flexDirection: "row",
     background: "white",
     backgroundColor: "transparent",
@@ -29,7 +44,7 @@ const useStyles = makeStyles({
   },
     menu_btn: {
     textDecoration: "none",
-    padding: "0px 20px 0px 20px",
+    padding: "0px 15px 0px 15px",
   },
   btn_grp: {
     padding: 10,
@@ -44,10 +59,16 @@ function Header(props) {
   return (
     <Grid container  className={classes.header_styles}>
       <Grid container className={classes.container_styles} >
-        <Grid item xs={2}>
-          <img src={logo} alt="Logo" />
+        <Grid item xs={3} >
+          <a href="/"  className={classes.logo_copy}>
+            <img src={logo} alt="Logo" id="branding" />
+            <Grid container direction="column" className={classes.logo_copy_text}>
+              <h1 className={classes.site_title}>Company Name</h1>
+              <small>Tagline goes here</small>
+            </Grid>
+          </a>
         </Grid>
-        <Grid item xs={4} className={classes.resetPadding}>
+        <Grid item xs={5} className={classes.resetPadding}>
           <AppBar position="static" className={classes.nav}>
             <ButtonGroup
               variant="contained"
